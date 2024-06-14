ba524b6a44d204cbd6e9cc0976e5e1dce25360ec
GitTaro <marui.yuta@nhosa.co.jp>
2024-06-14 18:15:42 +0900

    クラスの追加

---
+++ ShoppingSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoppingSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ShoppingSite/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+namespace ShoppingSite.Models
+{
+    public class User :IdentityUser<int>
+    {
+        public string Address { get; set; }
+        public int RviewId { get; set; }
+        public int PaymentId {  get; set; }
+        public int FavoriteId { get; set; }
+
+    }
+}
