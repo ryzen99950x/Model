1d3fb8d0aa082db1205069035c74acfeb09d3e49
GitTaro <marui.yuta@nhosa.co.jp>
2024-06-21 16:28:35 +0900

    商品の詳細の追加

--- ShoppingSite/obj/Debug/net8.0/ShoppingSite.AssemblyInfo.cs
+++ ShoppingSite/obj/Debug/net8.0/ShoppingSite.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af7df4fd6945a03a5def6bc0d4a12da10efdf456")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+480e9a9d30f0f5cc2d64919c729c20e2998ceb2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
